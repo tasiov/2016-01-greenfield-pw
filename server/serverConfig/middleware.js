1818ed8eb5639e6f428aeea9becc13e6c63d831e
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-17 19:22:27 -0800

    Add util module for extra functions, import express-session to create request sessions upon valid logins

--- server/serverConfig/middleware.js
+++ server/serverConfig/middleware.js
@@ -1,10 +1,16 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 
 module.exports = function(app, express) {
 	app.use(morgan('dev'));
 	app.use(bodyParser.urlencoded({extended: true}));
 	app.use(bodyParser.json());
-	app.use(express.static(__dirname + '/../../client'))
+	app.use(express.static(__dirname + '/../../client'));
+	app.use(session( {
+		secret: 'it\'s a secret',
+		resave: true,
+		saveUninitialized: true
+	}));
 }
