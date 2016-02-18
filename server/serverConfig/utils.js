c1ba8781cfbc8d37c566322a42b8bccd4ec70df2
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-17 19:31:26 -0800

    ExtractUserInfo method now return sample JSON object

--- server/serverConfig/utils.js
+++ server/serverConfig/utils.js
@@ -1,3 +1,8 @@
+var Promise = require('bluebird');
+var fs = require('fs');
+Promise.promisifyAll(fs);
+
+
 
 module.exports.checkUser = function(username, password, callback) {
 	if(username === 'username' && password === 'password') {
@@ -6,6 +11,12 @@ module.exports.checkUser = function(username, password, callback) {
 }
 
 module.exports.extractUserInfo = function(username, password, callback) {
-	callback(null, 'Info on user');
-
+	return fs.readFileAsync(__dirname + '../data/sampleGet.json')
+	.then(function(data) {
+		var userObj = JSON.parse(data);
+		return userObj;
+	})
+	.catch(function(err) {
+		return {Error: 0};
+	});
 }
