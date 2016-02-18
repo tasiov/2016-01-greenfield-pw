63efb72fec8c2fe5542d87ec4832b0a87a9dbdf6
Tasio Victoria <tasiovictoria@gmail.com>
2016-02-17 16:36:45 -0800

    inits client side using react

---
+++ client/components/App.js
@@ -0,0 +1,16 @@
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
