f42d200c68a7f31b817d5a49acc629abec99fe64
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-28 02:28:59 -0800

    Refactor Styling with List material ui

--- client/components/Main/Summary.jsx
+++ client/components/Main/Summary.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import {getNutritionInfo} from './NutritionCounter.jsx';
 import MacroPieChart from './MacroPieChart.jsx';
 import ProgressBar from './ProgressBar.jsx';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import Divider from 'material-ui/lib/divider';
 
 const Summary = ({user}) => {
 	if(Array.isArray(user.meals) && user.meals.length !== 0) {
@@ -25,17 +28,28 @@ const Summary = ({user}) => {
 
 		return (
 				<div className = 'summary'>
-					<p>Calories Consumed Today: {currDaySum['nf_calories']} </p>
-					<p>Protein Consumed Today: {currDaySum['nf_total_fat']} g</p>
-					<p>Carbohydrates Consumed Today: {currDaySum['nf_total_carbohydrate']} g</p>
-					<p>Fat Consumed Today: {currDaySum['nf_protein']} g</p>
+					<List subheader="Today's Nutrition Info">
+						<ListItem primaryText="Calories Consumed Today:" secondaryText= {currDaySum['nf_calories']} />
+						<Divider insert={true} />
+						<ListItem primaryText = 'Protein Consumed Today:' secondaryText={currDaySum['nf_total_fat']} g />
+						<Divider insert={true} />
+						<ListItem primaryText = 'Carbohydrates Consumed Today:' secondaryText={currDaySum['nf_total_carbohydrate']} g />
+						<Divider insert={true} />
+						<ListItem primaryText = 'Fat Consumed Today:' secondaryText={currDaySum['nf_protein']} g />
+						<Divider insert={true} />
+					</List>
 					<MacroPieChart macroPercents={currDayPerc} />	
 					<br></br>
-					<p>Daily Calorie consumption: {NFdailyAvg['nf_calories']}</p>
-					<p>Daily Protein consumption: {NFdailyAvg['nf_protein']} g</p>
-					<p>Daily Carbohydrate Consumption: {NFdailyAvg['nf_total_carbohydrate']} g</p>
-					<p>Daily Fat Consumption: {NFdailyAvg['nf_total_fat']} g</p>
+					<List subheader="Daily Nutrition Averages">
+						<ListItem primaryText="Daily Calorie consumption:" secondaryText= {NFdailyAvg['nf_calories']}/>
+						<Divider insert={true} />
+						<ListItem primaryText="Daily Protein consumption:" secondaryText= {NFdailyAvg['nf_protein']} />
+						<Divider insert={true} />
+						<ListItem primaryText="Daily Carbohydrate Consumption:" secondaryText= {NFdailyAvg['nf_total_carbohydrate']}/>
+						<Divider insert={true} />
+						<ListItem primaryText="Daily Fat Consumption:" secondaryText= {NFdailyAvg['nf_total_fat']}/>
 					<ProgressBar datedNutr={nutrByDate} />
+					</List>
 				</div>
 			);
 
