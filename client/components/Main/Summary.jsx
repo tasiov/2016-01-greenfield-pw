2220e9039d8e72e6ffb3900aec1492073f3fbb39
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-27 20:05:40 -0800

    Add pie chart with day MacroNutrient Breakdown

--- client/components/Main/Summary.jsx
+++ client/components/Main/Summary.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {getNutritionInfo} from './NutritionCounter.jsx';
+import {Pie} from 'react-chartjs';
 
 const Summary = ({user}) => {
 	if(Array.isArray(user.meals) && user.meals.length !== 0) {
@@ -21,6 +22,26 @@ const Summary = ({user}) => {
 		let currDayGramSum = currDaySum['nf_protein'] + currDaySum['nf_total_carbohydrate'] + currDaySum['nf_total_fat'];
 		let currDayPerc = _.mapValues(currDaySum, (gramAvg) => (gramAvg/currDayGramSum*100));
 		delete currDayPerc['nf_calories'];
+
+		let pieChartData = [
+			{ 
+				value: currDayPerc['nf_protein'],
+				color: '#F7464A',
+				highlight: '#FF545E',
+				label: 'Protein %'
+			}, {
+				value: currDayPerc['nf_total_carbohydrate'],
+				color: '#46BFBD',
+				highlight: '#5Ad3D1',
+				label: 'Carbs %'
+			}, {
+				value: currDayPerc['nf_total_fat'],
+				color: '#FDB45C',
+				highlight: '#FFC870',
+				label: 'Fat %'
+			}]
+
+
 		return (
 				<div className = 'summary'>
 					<p>Daily Number of Calories: {NFdailyAvg['nf_calories']}</p>
@@ -30,6 +51,7 @@ const Summary = ({user}) => {
 					<p>% Daily Average of Protein: {NFpercAvgMass['nf_protein']}</p>
 					<p>% Daily Average of Carbs: {NFpercAvgMass['nf_total_carbohydrate']}</p>
 					<p>% Daily Average of Fat: {NFpercAvgMass['nf_total_fat']}</p>
+					<Pie data={pieChartData}/>
 					<br></br>
 					<p>Current Day % of Protein: {currDayPerc['nf_protein']}</p>
 					<p>Current Day % of Carbs: {currDayPerc['nf_total_carbohydrate']}</p>
