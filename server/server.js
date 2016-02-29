03e897031ad0c8bb61e2eeb87330a5f63f3bd60e
Tasio Victoria <tasiovictoria@gmail.com>
2016-02-29 10:49:01 -0800

    adds comments to server file

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ var mongoose = require('mongoose');
 var Foods = require('./models/foods.js');
 var app = express();
 
+// middleware.js houses all the middleware that is run by express
 require(__dirname + '/serverConfig/middleware.js')(app, express);
+
+// routes.js defines the application's endpoints
 require(__dirname + '/serverConfig/routes.js')(app, express);
 
 mongoose.connect('mongodb://127.0.0.1/photogenicFood');
-//Above files configure everyting on the server. Clientside is configured in app.js in public
-
-
 
 app.listen(3000, function() {
 	console.log('listening on 3000');
