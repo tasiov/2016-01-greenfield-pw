d83a813b6c4631e304c97951460f6b8b4383f142
Orlando Castillo <orlandoc@sas.upenn.edu>
2016-02-29 16:45:55 -0800

    Commented Summary component

--- client/components/Main/Summary.jsx
+++ client/components/Main/Summary.jsx
@@ -6,13 +6,22 @@ import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 import Divider from 'material-ui/lib/divider';
 
+//Used to render the summary page
 const Summary = ({user}) => {
+
+	//If there are meals logged in the user objects and it's not zero length, then we calculate
+	//the relevant data, else we return a p element that says no meals have been logged
 	if(Array.isArray(user.meals) && user.meals.length !== 0) {
+
+		//Additional fields of information that I want the getNutrition info to extract
 		let additionalFields = ['nf_calcium_dv', 'nf_calories_from_fat', 'nf_cholesterol',
 														'nf_dietary_fiber','nf_iron','nf_monounsaturated_fat', 
 														'nf_polyunsaturated_fat', 'nf_saturated_fat', 'nf_sodium',
 														'nf_sugars', 'nf_trans_fatty_acid', 'nf_vitamain_a_dv',
 														'nf_vitamin_c_dv'];
+
+		//Collect all the meals in the userObject into a simple lookup Object called mealsByDate
+		//whose key is the date of consumption and whose values are an array of meals objects, (which log food eaten)
 		let dateFunctions = ['getDate', 'getMonth', 'getFullYear'];
 		let mealsByDate = _.transform(user.meals,(result, meal) => {
 			let mealDate = new Date(meal.eatenAt);
@@ -20,19 +29,31 @@ const Summary = ({user}) => {
 			result[dateString] = result[dateString] || [];
 			result[dateString].push(meal);
 		}, {});
+
+		//Use the getNutritionInfo function to map the array of meals at each date in mealsByDate to a single object
+		//with all the aggregate nutritional info for that day. That object is referred to as nutrByDate
 		let nutrByDate = _.mapValues(mealsByDate, mealsArr => getNutritionInfo(mealsArr, user.foods, additionalFields));
 
+		//Reduce the nutrional values in nutrByDate to one single object that has the key be a specific nutritional field
+		//and its value be the average for that field across all the dates in nutrByDate
 		let NFdailyAvg = _.values(nutrByDate).reduce( (prev, nutrObj, index) => {
 			return _.mergeWith({}, prev, nutrObj, (prevV, nutrV) => ((prevV || 0)*(index) + (nutrV || 0))/(index + 1));
 		});
 
+		//Grab the currDay's nutritional info by parsing todays date into a string and passing that string as a key to
+		//the nutr object. If it's undefined, then just return a blank nutr object
 		let currDaySum = nutrByDate[dateFunctions.map(func => (new Date())[func]()).join('-')] || getNutritionInfo([]);
+
+		//get the mass sum for the three main macro nutrients and get their percentage compositions of the total grams
+		//eaten using _.mapValues (note: there will be extra values in currDayPerc that have no nutritional utility)
 		let currDayGramSum = currDaySum['nf_protein'] + currDaySum['nf_total_carbohydrate'] + currDaySum['nf_total_fat'];
 		let currDayPerc = _.mapValues(currDaySum, (gramAvg) => (gramAvg/currDayGramSum*100));
 
+		//Round all numbers to a single decimal place
 		currDaySum = _.mapValues(currDaySum, (num) => num.toFixed(1));
 		NFdailyAvg = _.mapValues(NFdailyAvg, (num) => num.toFixed(1));
 
+		//Display all informaiton in two large lists shown below with graph components added
 		return (
 				<div className = 'summary'>
 					<div className = 'current-day-summary'>
